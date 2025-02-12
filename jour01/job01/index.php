59ee4ebab7410e63c5726cb4b5f1168a7f6a09fa
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-02-12 23:15:02 +0100

    feat: ajoute le job01 du jour01

---
+++ jour01/job01/index.php
@@ -0,0 +1,15 @@
+<?php 
+$str = "LaPlateforme";
+echo $str, "\n";
+$str2 = "Vive";
+$str3 = "!";
+echo "{$str2} {$str} {$str3}\n";
+$val = 6;
+echo $val, "\n";
+$val += 4;
+echo $val, "\n";
+$myBool = true;
+echo $myBool, "\n";
+$myBool = false;
+echo $myBool, "\n";
+?>
